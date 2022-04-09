cc7036363b7dd443b84f370a4267e9f16c8f8ea9
kleyberthsantos <73177733+kleyberthsantos@users.noreply.github.com>
2022-04-09 00:47:14 -0400

    creación de index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//1) Tener la variable react en scope
+import React from "react"
+
+//2) Tener la variable reactDOM en scope
+import ReactDOM from "react-dom"
+
+//3) Tener la variable App(la variable que contiene todo el codigo de la aplicacion) en scope
+function App() {return "Hola mi nombre es Kley"}
+
+//4) Hacer render de la App en el DOM
+ReactDOM.render(<App/>, document.getElementById("root"))
